5809725ace3e12a61932a681e29370ce915a1267
ritwik <ritwikdeshpande01@gmail.com>
2020-04-20 21:55:24 +0530

    Added Documentation

--- README.md
+++ README.md
@@ -1,19 +1,27 @@
-**Prerequisites before running**
-<br/>
-
-Python version :3.X
+**Installation guide**
+ 
  <br/>
+To execute using the web Platform 
+1. Install the prerequisite python packages. 
+a. Flask 
+b. Sci-kit learn 
+ 
+2. Clone the repository from GitHub: 
+https://github.com/ritwik-deshpande/ML-vs-COVID-19 
+ 
+3. Navigate to the ML v/s COVID19 folder 
+4. Run the ​ command 
 
-Install schkit learn 
-<br/>
+    python server.py​
 
-Install flask
-<br/>
+5. Log on to the http://127.0.0.1:5000/​ to access the web platform  
+ 
+ <br/>
+To execute directly from the terminal 
+1. Navigate to the ML-vs-COVID-19 directory to the directory where naive_bayes is stored. 
+2. Run the ​ command 
 
-
-
-To run the code :
-
-    python server.py
-
+    naive_bayes.py
+ 
+3. Enter the required input. You can take help from the Sample_Inputs file.
 
---
+++ naive_bayes.py
@@ -0,0 +1,186 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import re
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB 
+from sklearn import metrics
+from sklearn import preprocessing
+from flask import Flask,render_template
+from datetime import date,datetime
+import pickle
+from datetime import date
+
+encodings = dict()
+def getSymptomCategory(symptoms):
+	index = {
+	    'suspect':['fever','cold','flu','dizzyness','strong headache','dehydration'],
+	    'high_risk':['dry cough','breathlessness','high fever']
+	}
+
+
+	for word in symptoms.strip(',').split(','):
+		word = word.lower()
+		if word in index['suspect']:
+			return 'Suspect'
+		elif word in index['high_risk']:
+			return 'High_risk'
+	return 'Safe'
+
+def getNotesCategory(notes):
+	index = {
+	    "suspect":['mumbai','delhi','mh','up','maharastra','kerala','chennai','patient'],
+	    "high_risk":['airport','foreign','confirmed','dubai','iran','bahrain','us','covid','wuhan','china','uae','kenya','italy','spain','positive','hospital','saudi','thailand','usa','france','germany','uk','london','qatar','paris','lanka','middle','east','hospital','admitted','turkey','mecca','mexico','japan','indonesian','indonesia','philippines','singapore'],
+	}
+
+	for word in notes.strip().split():
+		word = word.lower().strip(',')
+		if word in index['suspect']:
+			return 'suspect'
+		elif word in index['high_risk']:
+			return 'high_risk'
+		else:
+			rexp = re.compile(r"P[0-9]+$")
+			if rexp.search(word):
+				return 'suspect'
+
+	return 'safe'
+
+
+
+def encode(df):
+
+	le = preprocessing.LabelEncoder()
+	global encodings
+	#encodings = dict()
+
+	df['gender'] = le.fit_transform(df['gender'])
+	gender_mapping = dict(zip(le.classes_, le.transform(le.classes_)))
+	encodings['gender'] = gender_mapping
+
+	df['notes_category'] = le.fit_transform(df['notes_category'])
+	notes_mapping = dict(zip(le.classes_, le.transform(le.classes_)))
+	encodings['notes'] = notes_mapping
+
+	df['Symptoms_category'] = le.fit_transform(df['Symptoms_category'])
+	symptom_mapping = dict(zip(le.classes_, le.transform(le.classes_)))
+	encodings['symptoms'] = symptom_mapping
+
+	df['detected_state'] = le.fit_transform(df['detected_state'])
+	state_mapping = dict(zip(le.classes_, le.transform(le.classes_)))
+	encodings['state'] = state_mapping
+
+	print(encodings)
+	print(df.head())
+	return
+
+
+def trainModel():
+
+	global encodings
+	df = pd.read_csv('Dataset/dataset.csv')
+	df = df.drop(['Symptoms','notes','current_status'], axis = 1)
+	df = df[df.days_to_change >= 0]
+	
+	encode(df)
+
+	
+
+	X = df.loc[:,'gender':'Symptoms_category']
+	y = df['category'] 
+
+
+	# splitting X and y into training and testing sets 
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1) 
+	  
+	# training the model on training set 
+	mnb = MultinomialNB() 
+	mnb.fit(X_train, y_train) 
+	  
+	# making predictions on the testing set 
+	print(X_test.shape)
+
+	y_pred = mnb.predict(X_test) 
+	  
+	# comparing actual response values (y_test) with predicted response values (y_pred) 
+	print("Gaussian Naive Bayes model accuracy(in %):", metrics.accuracy_score(y_test, y_pred)*100)
+
+	
+	# save the classifier
+	with open('my_dumped_classifier.pkl', 'wb') as fid:
+
+		pickle.dump(mnb, fid)
+
+	with open('my_encodings.pkl', 'wb') as eid:
+
+		pickle.dump(encodings, eid)
+
+	return
+
+
+    
+def predict(gender,age,notes,detected_state,symptoms,diagnosed_date):
+
+
+	#fetch the saved trained model from the pickel file
+	with open('my_encodings.pkl', 'rb') as eid:
+		encodings = pickle.load(eid)
+		 
+	with open('my_dumped_classifier.pkl', 'rb') as fid:
+		mnb = pickle.load(fid)
+
+	current_time = datetime.now()
+
+	current_date = date(current_time.year,current_time.month,current_time.day)
+
+
+	diagnosed_time = diagnosed_date.split('/')
+	diagnosed_date = date(int(diagnosed_time[2]),int(diagnosed_time[1]),int(diagnosed_time[0]))
+
+
+	age = int(age)//10
+
+	days_to_change = (current_date - diagnosed_date).days//5
+
+	print(days_to_change)
+	test = [[encodings['gender'][gender], encodings['state'][detected_state],age,days_to_change,encodings['notes'][getNotesCategory(notes)],encodings['symptoms'][getSymptomCategory(symptoms)]]]
+	print(test)
+	print(mnb.predict(test))
+
+	return mnb.predict(test)
+
+if __name__ == '__main__':
+
+	trainModel()
+
+	print("Enter your Gender (M / F)\n")
+	gender = input()
+
+	print("Enter your Age\n")
+	age = int(input())
+
+	print("Enter your Travel History or patient you have come in contact with\n")
+	notes = input()
+
+	print("Enter the name of State you live in\n")
+	state = input()
+
+	print("Enter your Symptoms\n")
+	symptoms = input()
+
+	print("Enter the date you started feeling unwell (DD/MM/YYY)\n")
+	d = input()
+
+	ans = str(predict(gender,age,notes,state,symptoms,d)[0])
+	print(ans)
+
+	print('The patient belongs to the Category')
+	if ans == '2.0' or ans =='2':
+		print('Suspected')
+
+	elif ans == '1.0' or ans =='1':
+		print('High Risk')
+	else:
+		print('Safe')
+
+
